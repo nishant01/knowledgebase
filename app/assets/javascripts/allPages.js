834cc6de78fa975eb1a650205b27d3d66aa127c8
Nishant <nishant01@gmail.com>
2014-07-11 09:54:21 +0530

    add js file

--- app/assets/javascripts/allPages.js
+++ app/assets/javascripts/allPages.js
@@ -3,4 +3,36 @@ function submitFormPost(theForm, theFunction ) {
     $.post(theForm.action, $("#" + theForm.id).serialize(),theFunction,'html');
 }
 function fillAjaxHolder(theURL) {
-    $.get(theURL,      function(data, statusText, xhrObject) {        $('#ajaxHolder').html(data);      });      return false;  }  function writeAjaxHolder(theContent) {    $('#ajaxHolder').html(theContent);    return false;  }  $(".ajaxDeleteLink").live('click',function(event){    event.preventDefault();    $.post(event.target,    "_method=DELETE",    function(data) {      writeAjaxHolder(data);    },    'html');  });  $('.ajaxLink').live('click',function(event){    event.preventDefault(); // Prevent link from following its href    fillAjaxHolder(event.target);    return false;  });  $('.ajaxSubmit').live('click',function(event){    event.preventDefault(); // Prevent link from following its href    submitFormPost(event.target.form,      function(data) {        writeAjaxHolder(data);      }    );  });
+    $.get(theURL,
+        function(data, statusText, xhrObject) {
+            $('#ajaxHolder').html(data);      });
+    return false;
+}
+
+function writeAjaxHolder(theContent) {
+    $('#ajaxHolder').html(theContent);
+    return false;
+}
+
+$(".ajaxDeleteLink").live('click',function(event){
+    event.preventDefault();
+    $.post(event.target,    "_method=DELETE",
+        function(data) {
+            writeAjaxHolder(data);
+        },    'html');
+});
+
+$('.ajaxLink').live('click',function(event){
+    event.preventDefault(); // Prevent link from following its href
+    fillAjaxHolder(event.target);
+    return false;
+});
+
+$('.ajaxSubmit').live('click',function(event){
+    event.preventDefault(); // Prevent link from following its href
+    submitFormPost(event.target.form,
+        function(data) {
+            writeAjaxHolder(data);
+        }
+    );
+});
