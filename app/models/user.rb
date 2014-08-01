23b7e5fbf2496d066d34f9c35b0200e2865fd87f
Nishant <nishant01@gmail.com>
2014-08-01 19:17:16 +0530

    added user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
     has_many :roles, :through => :user_roles
     has_many :articles
     belongs_to :departments
+    after_save :check_and_add_manager
 
     # Include default devise modules. Others available are:
     # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
@@ -14,4 +15,16 @@ class User < ActiveRecord::Base
 
     attr_accessible :email, :password, :password_confirmation, :remember_me, :role_ids, :name, :surname, :contact,
                     :address,:department_id, :is_manager
+
+    def self.check_and_add_manager
+        if self.is_manager == 1
+            dep_manager = DepartmentManager.new
+            dep_manager.department_id = self.department_id
+            dep_manager.manager_id = self.id
+            dep_manager.save
+            return true
+        else
+            return false
+        end
+    end
 end
